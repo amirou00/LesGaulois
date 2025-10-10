a6c29c59df3a829c3427f360e478719de332c329
amirou00 <ah2forghani@gmail.com>
2025-10-10 11:34:18 +0200

    finir le main de la classe Village (on est en page 6 du tp2)

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -21,15 +21,30 @@ public class Village {
 	}
 	
 	public void ajouterVillageois(Gaulois gaulois) {
-		
+		if(nbVillageois < villageois.length) {
+			villageois[nbVillageois] = gaulois;
+			nbVillageois++;
+			gaulois.setVillage(this);
+		}
+		else {
+			System.out.println("!!!Le viallage est full chef!!!");
+		}
 	}
 	
 	public Gaulois trouverVillageois(int numVillageois) {
-		
+		if (1 <= numVillageois && numVillageois <= nbVillageois) {
+			return villageois[numVillageois - 1];
+		}
+		System.out.println("“Il n’y a pas autant d’habitants dans notre village !");
+		return null;
 	}
 	
 	public void afficherVillage() {
 		
+	}
+	
+	public static void main(String[] args) {
+		Gaulois abraracourcix = new Gaulois("Abraracourcix", 6);
 	}
 
 }
